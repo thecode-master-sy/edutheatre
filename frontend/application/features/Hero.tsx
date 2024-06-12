702362dcf1636ed16afb78059da7e6549aa1a23b
kacy236 <ndubuisik216@gmail.com>
2024-06-12 12:31:03 -0700

    new changes

---
+++ frontend/application/features/Hero.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='max-container padding-container flex flex-col 
+    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+        <div className='hero-map'/>
+
+        {/* LEFT */}
+
+        <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Hero

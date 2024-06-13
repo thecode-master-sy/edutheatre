3a03f8a4f839bab40c5fce6e1c049a33e80f4723
kacy236 <ndubuisik216@gmail.com>
2024-06-13 10:49:14 -0700

    another commit

--- frontend/application/features/Guide.tsx
+++ frontend/application/features/Guide.tsx
@@ -25,7 +25,29 @@ const Guide = () => {
           className="w-full object-cover object-center 2xl:rounded-5xl"
         />
 
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image 
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className='flex w-full flex-col'>
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50"></p>
+              </div>
+              <p className="bold-20 mt-2">lorem ipsum</p>
+            </div>
 
+            <div className='flex w-full flex-col'>
+              <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">lorem ipsum</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )

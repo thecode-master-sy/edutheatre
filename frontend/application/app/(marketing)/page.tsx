e961d24920cc5a06e81ad05e6a202ee7b4cf79e2
kacy236 <ndubuisik216@gmail.com>
2024-06-13 20:05:14 -0700

    i want to sleep

--- frontend/application/app/(marketing)/page.tsx
+++ frontend/application/app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 import Text from "@/features/feature-marketing/ui/Text"
 import Hero from "@/features/feature-marketing/ui/Hero";
 import Layout from "@/features/feature-marketing/ui/Layout"
+import Guide from "@/features/feature-marketing/ui/Guide"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
      <Hero/>
      <Text/>
      <Layout/>
+     <Guide/>
 
     </>
   )
